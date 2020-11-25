c373b1750c91838549972580ad6d782acdec9499
Driptaroop Das <driptaroop.das@dkbcodefactory.com>
2020-11-25 18:10:30 +0100

    upgrade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	val kotlinVersion = "1.4.10"
+	val kotlinVersion = "1.4.20"
 	id("org.springframework.boot") version "2.4.0"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version kotlinVersion
